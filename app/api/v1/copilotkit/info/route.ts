2b12ba6c383b2549e27631c4fce389831f46a82d
Bag-zy <bagumajonah3@gmail.com>
2026-01-06 02:40:18 +0300

    update

---
+++ app/api/v1/copilotkit/info/route.ts
@@ -0,0 +1,17 @@
+import { copilotkitHandler } from '../../../copilotkit/_handler'
+
+export const runtime = 'nodejs'
+
+function rewriteToNonV1(req: Request) {
+  const url = new URL(req.url)
+
+  if (url.pathname.startsWith('/api/v1/copilotkit')) {
+    url.pathname = url.pathname.replace('/api/v1/copilotkit', '/api/copilotkit')
+  }
+
+  return new Request(url.toString(), req)
+}
+
+export async function GET(req: Request) {
+  return copilotkitHandler.handleRequest(rewriteToNonV1(req))
+}
